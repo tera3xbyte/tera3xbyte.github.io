40585915299187a83af3a4f488ba2bb33d908d7f
Nathan Thirza <teraa3xbyte@yahoo.com>
2022-04-29 21:10:46 +0800

    Font picking, mobile view navbar improvements, directory cleaning, added typwritejs library

---
+++ src/js/nav-toggle.js
@@ -0,0 +1,6 @@
+const nav = document.querySelector('nav > ul');
+const menuToggle = document.querySelector('.menu-toggle > input');
+menuToggle.addEventListener('click', function() {
+	nav.classList.toggle('slide');
+
+});
--- src/js/typing.js
+++
@@ -1,6 +0,0 @@
-import Typewriter from 'typewriter-effect/dist/core';
-
-new Typewriter('#write', {
-  strings: ['Hello', 'World'],
-  autoStart: true,
-});
