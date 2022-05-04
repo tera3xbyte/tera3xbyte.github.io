1ba198bfeda2288552f14020687d35069d07073e
Nathan Thirza <teraa3xbyte@yahoo.com>
2022-05-04 21:12:49 +0800

    - fix merging issues (turns out rebasing) - installed prettier - fixed #id anchor

--- src/js/nav-toggle.js
+++ src/js/nav-toggle.js
@@ -1,6 +1,5 @@
-const nav = document.querySelector('nav > ul');
-const menuToggle = document.querySelector('.menu-toggle > input');
-menuToggle.addEventListener('click', function() {
-	nav.classList.toggle('slide');
-
+const nav = document.querySelector("nav > ul");
+const menuToggle = document.querySelector(".menu-toggle > input");
+menuToggle.addEventListener("click", function () {
+  nav.classList.toggle("slide");
 });
--- src/js/typewrite-platforms.js
+++ src/js/typewrite-platforms.js
@@ -1,18 +1,16 @@
-var idPlatform = document.getElementById('platforms');
-var platforms = ['CLI tools.', 'Situs Web.'];
+var idPlatform = document.getElementById("platforms");
+var platforms = ["CLI tools.", "Situs Web."];
 
 var tw = new Typewriter(idPlatform, {
-	loop: true,
-	delay: 75,
+  loop: true,
+  delay: 75,
 });
 
 for (var i = 0; i < platforms.length; i++) {
-
-	tw
-	.pauseFor(2500)
-	.typeString(platforms[i])
-	.pauseFor(1000)
-	.deleteChars(platforms[i].length)
-	.pauseFor(1000)
-	.start();
+  tw.pauseFor(2500)
+    .typeString(platforms[i])
+    .pauseFor(1000)
+    .deleteChars(platforms[i].length)
+    .pauseFor(1000)
+    .start();
 }
